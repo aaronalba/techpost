99611141140c12a0a78e53fa6e98ac15610edb8a
aaronalba <aaronalba.dev@gmail.com>
2022-09-01 23:25:15 +0800

    add SearchFragment

---
+++ app/src/main/java/com/aaron/techpost/ui/SearchFragment.kt
@@ -0,0 +1,77 @@
+package com.aaron.techpost.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.aaron.techpost.databinding.FragmentSearchBinding
+import com.aaron.techpost.ui.viewmodel.SearchViewModel
+
+class SearchFragment : Fragment() {
+
+    // the view model for this fragment
+    private val viewModel: SearchViewModel by viewModels()
+
+    /**
+     * View binding property to access the views. Valid only between
+     * onCreateView() and onDestroyView().
+     */
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.hasResult.observe(viewLifecycleOwner) {
+            it?.let { hasResult ->
+                // hide the no result message if there is a received search result
+                makeNoResultVisible(!hasResult)
+
+                // show the recyclerview that will display the search result
+                makeRecyclerViewVisible(hasResult)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    /**
+     * Toggles the visibility of the no result message.
+     */
+    private fun makeNoResultVisible(b: Boolean) {
+        val visible = if (b) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+        binding.noResultImage.visibility = visible
+        binding.noResultHeader.visibility = visible
+        binding.noResultSubheader.visibility = visible
+    }
+
+    /**
+     * Toggles the visibility of the list of results.
+     */
+    private fun makeRecyclerViewVisible(b: Boolean) {
+        val visible = if (b) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+        binding.searchRecyclerView.visibility = visible
+    }
+}
