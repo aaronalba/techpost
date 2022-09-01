fa0d453066bb41c5270ca8c7f659725229e79015
aaronalba <aaronalba.dev@gmail.com>
2022-09-01 23:55:50 +0800

    add starred fragment

---
+++ app/src/main/java/com/aaron/techpost/ui/StarredFragment.kt
@@ -0,0 +1,32 @@
+package com.aaron.techpost.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.aaron.techpost.databinding.FragmentStarredBinding
+
+class StarredFragment : Fragment() {
+
+    /**
+     * View binding property to access the views. Valid only between
+     * onCreateView() and onDestroyView().
+     */
+    private var _binding: FragmentStarredBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentStarredBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
